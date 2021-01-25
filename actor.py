d2e64717c0455a90c41fb2190ba945fb8f7a7a25
João Henrique <jhenriquelc@gmail.com>
2021-01-25 17:55:46 -0300

    fixed not working code (i think) and added .gitignore

---
+++ actor.py
@@ -0,0 +1,37 @@
+class actor():
+    def __init__(self, start, limits, barriers):
+        self.coords = start
+        self.limits = limits
+        self.barriers = barriers
+        done = False
+    
+    def canmove(self, direction):
+        moving = self.coords
+        if(direction == 'up'):
+            moving[1] -= 1
+        elif(direction == 'right'):
+            moving[0] += 1
+        elif(direction == 'down'):
+            moving[1] += 1
+        elif(direction == 'left'):
+            moving[0] -= 1
+        
+        if((moving[0] > self.limits[0]) or (moving[1] > self.limits[1]) or (-1 in moving) or (moving in self.barriers)):
+            return False
+        else:
+            return True
+
+    def move(self, direction):
+        if(direction == 'up'):
+            if self.canmove('up'):
+                self.coords[1] += 1
+
+        elif(direction == 'right'):
+            if self.canmove('right'):
+                self.coords[0] += 1
+        elif(direction == 'down'):
+            if self.canmove('down'):
+                self.coords[1] -= 1
+        elif(direction == 'left'):
+            if self.canmove('left'):
+                self.coords[0] -= 1

dbf95e3d801569ce55b7545fd18f54e5568b38b5
João Henrique <jhenriquelc@gmail.com>
2021-01-31 16:23:57 -0300

    Fixed inconsistencies in Board.draw() Finished adding functions in gameloop.py, still buggy Fixed board border collision Updated README.md

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 
+  
+
 # coords-py
 
+  
+
 A shitty top-down view board with a thingy you can move made in python to work in the terminal
+
+  
 
   
 
@@ -9,23 +15,51 @@ I'm currently having problems with movement. Any help is more than enough.
 
   
 
+  
+
 *Please only use my code if you're going to make your program free and open-source too, thanks. (I'm not enforcing it)*
+
+  
 
   
 
 ## How-to
 
   
+  
+
+## Dependencies
+
+  
+
+- pip modules: `clear-screen` and `getch`*
+
+To install them use:
+
+`pip install clear-screen getch`
+or
+`pip3 install clear-screen getch`
+
+*only needed for the example in `gameloop.py`
+  
 
 ### Board
+
+  
 
 The `Board` class creates a grid with customizable size, barriers (experimental), and an `Actor` object.
 
   
 
+  
+
 The Actor object holds a position (``Actor.coords``) and a function to move it (``Actor.move()``) which requires an direction represented by a string with its name.
 
+  
+
 Example: ``Actor.move('left')``
+
+  
 
   
 
@@ -33,22 +67,38 @@ Example: ``Actor.move('left')``
 
   
 
+  
+
 `board.draw()` writes the board line by line on the terminal, currently working on a way for the results to be returned by a function instead of printed
+
+  
 
   
 
 #### Coords
 
+  
+
 Coordinates are represented by a list with two numbers, line and column.
 
-An example: ``board.actor.coords = [5, 3]`` .
+  
+
+An example: `board.actor.coords = [5, 3]` .
+
+  
 
   
 
 *Lines are counted from top to bottom and begin with 0.*
 
+  
+
 #### Barriers list
+
+  
 
 Barriers are contained in a list of coords.
 
-Example: `` barriers = [[4, 5], [1, 4], [0, 3]]`` .
+  
+
+Example: ` barriers = [[4, 5], [1, 4], [0, 3]]` .
--- board.py
+++ board.py
@@ -1,7 +1,6 @@
-from actor import Actor
-
 class Board:
     def __init__(self, start, limits, barriers):
+        from actor import Actor
         self.limits = limits
         self.actor = Actor(start, limits, barriers)
         self.barriers = barriers
@@ -10,8 +9,8 @@ class Board:
         line = 0
         column = 0
         buffer = ''
-        while line < self.limits[0]:
-            while(column < self.limits[1]):
+        while line < self.limits[1]:
+            while(column < self.limits[0]):
                 if [column, line] in self.barriers:
                     buffer += '☒'
                 elif [column, line] == self.actor.coords:
@@ -23,83 +22,3 @@ class Board:
             print(buffer)
             buffer = ''
             line += 1
-
-# TODO: find a *reasonable* way to not have to copy&paste draw() from class Board
-def pre_draw(limits, actor_coords, barriers):
-    line = 0
-    column = 0
-    buffer = ''
-    while line < limits[0]: # draws all lines
-        while(column < limits[1]): # draws all columns
-            if [column, line] in barriers:
-                buffer += '☒'
-            elif [column, line] == actor_coords:
-                buffer += 'o'
-            else:
-                buffer += '·'
-            column += 1
-        column = 0
-        print(buffer)
-        buffer = ''
-        line += 1
-
-def getlimits():
-    while(True): # break on line 42
-        size = input('Set the size of the grid: ')
-        if size == 'h':
-            print('input two positive integers separated by space')
-            continue
-        # errors expected from user input xd
-        try:
-            size = size.split()
-            # with the input separated into a list, convert expected items to integers
-            size[0] = int(size[0])
-            size[1] = int(size[1])
-            if size[0] < 0 or size[1] < 0:
-                raise IndexError('Grid size should be positive')
-            else:
-                break
-        except:
-            print('invalid input, try again')
-            print('enter two positive integers separated by space.')
-            continue
-    return size
-
-def getstart(limits):
-    while(True):
-        start = input('Enter the initial position of the actor: ')
-        if start == 'h':
-            print('input two positive integers separated by space')
-            continue
-        if start == 'p':
-            pre_draw(limits, [], [])
-            continue
-        # errors expected from user input… again.̣
-        try:
-            start = start.split()
-            start[0] = int(start[0])
-            start[1] = int(start[1])
-            if start[0] < 0 or start[1] < 0:
-                raise IndexError('Positions must be positive')
-            elif start[0] >= limits[0] or start[1] >= limits[1]:
-                raise IndexError('Actor position can\'t be outside the grid')
-            else:
-                break
-        except:
-            print('invalid input, try again')
-            print('enter two positive integers separated by space,')
-            print('the coords given must fall inside the grid')
-    return start
-
-def getbarriers(limits, start): # TODO: implement functionality in getbarriers()
-    return []
-
-# TODO: walk through the process of creating a board with the user
-if __name__ == '__main__' :    
-    print('Input "h" for help')
-    print('!THIS PART OF THE CODE IS IMCOMPLETE!')
-    limits = getlimits()
-    print('Input "p" to see how the board currently looks like')
-    start = getstart(limits)
-    barriers = getbarriers(limits, start)
-    print(limits, start, barriers)

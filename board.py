060ec2b1f37a155af167f674a74459d054aee020
João Henrique <jhenriquelc@gmail.com>
2021-02-19 16:37:08 -0300

    Made Board.draw() prettier the code is kinda spaghetti but it works fine

--- board.py
+++ board.py
@@ -12,14 +12,26 @@ class Board:
         self.empty_char = ' '
         self.barrier_char = 'ø'
 
+        self.corner_char = '#'
+        self.wall_char = '|'
+        self.lid_char = '-'
+
+    @property
+    def ylimit(self):
+        return self.limits[1]
+
+    @property
+    def xlimit(self):
+        return self.limits[0]
+
     @property
     def out_list(self):
         cache = []
         line = 0
         column = 0
         buffer = ''
-        while line < self.limits[1]:
-            while(column < self.limits[0]):
+        while line < self.ylimit:
+            while(column < self.xlimit):
                 if [column, line] in self.barriers:
                     buffer += self.barrier_char
                 elif [column, line] == self.player:
@@ -33,8 +45,32 @@ class Board:
             line += 1
         return cache
 
+    @property
+    def out_pretty_list(self):
+        cache = []
+        out_list = self.out_list
+        for y in range(self.ylimit + 2):
+            buffer = ''
+            for x in range(self.xlimit + 2):
+                # if on top or bottom
+                if y == 0 or y == self.ylimit + 1:
+                    # and on the sides, aka on a corner
+                    if x == 0 or x == self.xlimit + 1:
+                        buffer += self.corner_char
+                    # on top or bottom, but not on a side
+                    else:
+                        buffer += self.lid_char
+                # if not on top on bottom, but on a side
+                elif x == 0 or x == self.xlimit + 1:
+                    buffer += self.wall_char
+                # if on middle
+                else:
+                    buffer += out_list[y - 1][x - 1]
+            cache.append(buffer)
+        return cache
+
     def draw(self):
-        for line in self.out_list:
+        for line in self.out_pretty_list:
             print(line)
 
     def canmove(self, direction):
@@ -50,8 +86,8 @@ class Board:
 
         # validations
         onbarrier = moving in self.barriers
-        pastx = moving[0] >= self.limits[0]
-        pasty = moving[1] >= self.limits[1]
+        pastx = moving[0] >= self.xlimit
+        pasty = moving[1] >= self.ylimit
         negative = -1 in moving
         blocked = moving in self.barriers
 
